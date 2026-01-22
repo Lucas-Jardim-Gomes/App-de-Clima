e869877e5d0e5cf9eb8cd681c9f2b13e0d8950ad
Lucas-Jardim-Gomes <113186715+Lucas-Jardim-Gomes@users.noreply.github.com>
2026-01-22 16:01:55 -0300

    adiciona o if e const url da api
    adicionei um if para que emita um alerta caso o usuario deixe o texto em branco na busca.  adicionei a chave da api e também a const url

--- App Clima/js/script.js
+++ App Clima/js/script.js
@@ -1,5 +1,18 @@
+const apiKey = 'VBjmr+d3$9=X,Qf'
+
+
+
 function buscarClima() {
     const cidade = document.getElementById('cidade').value;
 
-    alert('Buscando o clima para ' + cidade);
+
+    if (cidade === '') {
+        alert('Por favor, digite o nome de uma cidade.');
+        return
+    }
+
+
+    const url = 'https://api.openweathermap.org/data/2.5/weather?q=${cidade}&appid=${apiKey}&units=metric&lang=pt_br';
+
+    alert(url);
 }

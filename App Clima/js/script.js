ecfbcd5facb03e1c8db2f4a7cd0c3ae9ef0cea0d
Seu Nome <lukagomz@gmail.com>
2026-03-13 05:47:31 -0300

    adiciona controle de erro/ campo vazio

--- App Clima/js/script.js
+++ App Clima/js/script.js
@@ -1,18 +1,19 @@
-const apiKey = 'VBjmr+d3$9=X,Qf'
-
-
 
 function buscarClima() {
     const cidade = document.getElementById('cidade').value;
 
+// Controle de erro para campo vazio
 
-    if (cidade === '') {
-        alert('Por favor, digite o nome de uma cidade.');
+    if (cidade.trim() === '') {
+        alert('⚠️ Por favor, digite o nome de uma cidade.');
         return
     }
+
+    
 
 
     const url = 'https://api.openweathermap.org/data/2.5/weather?q=${cidade}&appid=${apiKey}&units=metric&lang=pt_br';
 
     alert(url);
 }
+   
